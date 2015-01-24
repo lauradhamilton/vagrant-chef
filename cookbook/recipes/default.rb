a9502c219eb26e317da18e39f451de336733f971
Laura <lauradhamilton@gmail.com>
2015-01-24 15:32:46 -0600

    Set up the forwarded port

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -1,4 +1,11 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 include_recipe "rbenv::rbenv_vars"
-rbenv_ruby "2.1.1"
+
+rbenv_ruby "2.1.1" do
+  global true
+end
+
+rbenv_gem "bundler" do
+  ruby_version "2.1.1"
+end
