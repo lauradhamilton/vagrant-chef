13dbf3654f7f2b3e9e1f28bb7c849f521e36c03b
Laura <lauradhamilton@gmail.com>
2015-01-25 14:34:09 -0600

    Adding tests to check that the bundler gem is installed and that the mysqld service is running

--- cookbook/test/integration/wdiy/serverspec/default_spec.rb
+++ cookbook/test/integration/wdiy/serverspec/default_spec.rb
@@ -7,3 +7,13 @@ set :path, '/opt/rbenv/bin:/opt/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/
 describe command('ruby --version') do
   its(:stdout) { should include '2.1.1' }
 end
+
+# Verify that bundler is installed
+describe package('bundler') do
+  it {should be_installed.by('gem') }
+end
+
+# Verify that MySQL service is running
+describe service('mysqld') do
+  it {should be_running }
+end
